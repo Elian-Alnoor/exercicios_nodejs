7faa46af05915af641ebd08ec73c0497239bfa52
unknown <elian.91@hotmail.com>
2020-06-19 19:59:44 -0300

    atividade 2 resolvida

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,9 @@
-// ## Importe a biblioteca ##
+let rs = require("readline-sync") 
+let per1 = rs.question("Usa mascara quando for sair da casa? sim ou não \n")
+let per2 = rs.question("Lava as mãos frequentemente? \n")
 
-
-
-// ## Faça o código ##
+if (per1 == "sim" && per2 == "sim"){
+    console.log("Está de parabéns")
+} else {
+    console.log("Tem que se cuidar")
+}
