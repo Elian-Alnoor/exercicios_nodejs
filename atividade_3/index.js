ffda9dee47d857ec4009e1552cd8eb6763d2cbc5
unknown <elian.91@hotmail.com>
2020-06-19 20:19:03 -0300

    atividade 3 resolvida

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,12 @@
-// ## Importe a biblioteca ##
+let rs = require("readline-sync")
+
+let lado1 = rs.questionFloat("informe o comprimento do primeiro lado:  \n")
+ let lado2 = rs.questionFloat("informe o comprimento do segundo lado:  \n")
+
+ let area = lado1 * lado2
+
+ console.log("A área do quadrado é:  " + area + " m²")
 
 
 
-// ## Faça o código ##
+
