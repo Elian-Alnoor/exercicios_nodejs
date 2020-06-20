1d2a738a98c0f9b63e27bf598df6e52ac46a9ba7
unknown <elian.91@hotmail.com>
2020-06-19 21:08:49 -0300

    atividade 2 resolvida

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,10 @@
-// ## Importe a biblioteca ##
+let rs = require("readline-sync")
 
+let numvoo = rs.questionInt("informe o número do seu voo:  \n")
 
+let fileira = rs.question("Em qual fileira deseja sentar:  \n")
 
-// ## Faça o código ##
+console.log("========================")
+console.log("seu voo é número: " + numvoo)
+console.log("sua fileira é: " + fileira)
+
